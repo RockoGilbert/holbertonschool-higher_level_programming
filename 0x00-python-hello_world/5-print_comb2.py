a640fd882ffbdfa4e157729559f157a6f7ca9427
RockoGilbert <evolve2122@gmail.com>
2021-09-29 21:09:10 -0500

    solved for i in range

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
-for count in range(0, 99):
-    print("{:02d}".format(count), end=', ')
-    print(99)
+for i in range(100):
+    if i != 99:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{:02d}".format(i))
