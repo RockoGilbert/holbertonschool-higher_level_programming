6e7a77cd43f186a19b600388553b24b1e1e5dd74
RockoGilbert <evolve2122@gmail.com>
2021-10-01 16:33:16 -0500

    fixed x in range

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    return [False if x % 2 else True for x in my_list if my_list].
+    if not my_list:
+        return None
+    flag = list(my_list)
+    for x in range(len(my_list)):
+        if my_list[x] % 2 == 0:
+            flag[x] = True
+        else:
+            flag[x] = False
+    return flag
