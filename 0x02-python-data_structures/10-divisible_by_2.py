10102b8a68caa0f6c08797f6a28f9787962f43ba
RockoGilbert <evolve2122@gmail.com>
2021-10-01 14:08:44 -0500

    function that divides by 2

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    if not my_list:
+        return None
+    new = list(my_list)
+    for x in range(len(my_list)):
+        if my_list[x] % 2 == 0:
+            new[x] = True
+        else:
+            new[x] = False
+            return flag
