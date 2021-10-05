8ba739bb38708327ff5369bc60c0ba1b2cec36e9
RockoGilbert <evolve2122@gmail.com>
2021-10-05 18:04:16 -0500

    Defined square by size

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Class square defined by size """
+""" Creates a class square with an attribute of size """
 
 
 class Square:
     """ Defines an object by it's size """
-    def __init__(self)
+    def __init__(self, size):
         self.__size = size
