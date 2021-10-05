599f068f94d51c404395eec944210351e92b4825
RockoGilbert <evolve2122@gmail.com>
2021-10-04 20:52:17 -0500

    replace values

--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    newList = my_list.copy()
-    for x in range(len(newList)):
-        if newList[x] == search:
-            newList[x] = replace
-        return (newList)
-
+    new_list = my_list[:]
+    for x in range(len(new_list)):
+        if new_list[x] == search:
+            new_list[x] = replace
+    return new_list
