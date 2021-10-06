91369c2c14e01a575c6b621a9deff7c98aa7f713
RockoGilbert <evolve2122@gmail.com>
2021-10-05 21:23:56 -0500

    Class Square that defines a square

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
-""" Defines a class Square by size"""
+""" Module Sqaure """
 
 
 class Square:
-    """ Defines an object size """
+    """ Square class defined by geometric shap"""
+    
+        Attributes:
+            size (int): Size of square
+    
     def __init__(self, size=0):
-        self.__size = size
-        @property
-        def size(self):
-            return self.__size
-        @size.setter
-        def size(self, value):
-            self.__size = value
-            if type(value) is not int:
+         """initializes the square"""
+        Args:
+        size (int): size of a side of the square
+        Raises:
+            TypeError: if size is not int
+            ValueError: size less than 0
+            Returns:
+                None
+            if type(size) is not int:
                 raise TypeError("size must be an integer")
-            if value < 0:
+            elif size < 0:
                 raise ValueError("size must be >= 0")
-            def area(self):
-                return self.__size * self.__size
+            else:
+                self.__size = size
+
+
+    def area(self):
+    """set square square area"""
+
+    Return:
+        the current square area (int)
+
+    return self.__size ** 2
