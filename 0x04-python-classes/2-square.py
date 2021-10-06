ab7b16aa43cfcfa5bccd6ec0f1ceaa30704915e0
RockoGilbert <evolve2122@gmail.com>
2021-10-05 20:47:14 -0500

    Defined a function by size

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -3,16 +3,13 @@
 
 
 class Square:
-    """Defines size of object"""
+    """ Defines """
     def __init__(self, size=0):
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0: 
+        if size < 0:
             raise ValueError("size must be >= 0")
 
-
-"""Defines square area"""
-def area(self, size=0):
-    sr_area = self.__size ** 2
-    return sr_area
+    def area(self):
+        return self.__size * self.__size
