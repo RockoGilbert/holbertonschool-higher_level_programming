797489aae0b9feac3f784b0005dcc71732363e35
RockoGilbert <evolve2122@gmail.com>
2021-10-06 09:09:17 -0500

    changed variable

--- 0x03-python-more_data_structures/2-uniq_add.py
+++ 0x03-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     newList = []
-    store = 0
+    keep = 0
     for x in my_list:
         if x not in newList:
             newList.append(x)
     for n in newList:
-        store += n
-    return store
+        keep += n
+    return keep
