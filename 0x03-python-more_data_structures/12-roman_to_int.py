a4cc1c6feaeebe56540256e323f93ad9d69b1674
RockoGilbert <evolve2122@gmail.com>
2021-10-06 20:24:16 -0500

    fixed pep8

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,18 @@
+#!/usr/bin/python3
 def roman_to_int(roman_string):
     vals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
     if type(roman_string) == str:
         j = iter(list(roman_string[1:]))
         for i in roman_string:
-           k = next(j, i)
-           try:
-               check = vals[i] < vals[k]
-           except KeyError:
-               return 0
-           finally:
-               if check:
-                   total -= vals[i]
-               else:
-                   total += vals[i]
-                   
+            k = next(j, i)
+            try:
+                check = vals[i] < vals[k]
+            except KeyError:
+                return 0
+            finally:
+                if check:
+                    total -= vals[i]
+                else:
+                    total += vals[i]
     return total
