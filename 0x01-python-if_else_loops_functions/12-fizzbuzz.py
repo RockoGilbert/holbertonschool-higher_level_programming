27234ab5a030c51b894bde1681f38620a41e7f5f
RockoGilbert <evolve2122@gmail.com>
2021-10-08 14:54:25 -0500

    changed if else

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 101):
-
-        if i % 3 != 0 and i % 5 != 0:
-            print(i, end='')
-
+    for num in range(1, 101):
+        if (num % 15 == 0):
+            print("FizzBuzz ", end="")
+        elif (num % 3 == 0):
+            print("Fizz ", end="")
+        elif (num % 5 == 0):
+            print("Buzz ", end="")
         else:
-            if i % 3 == 0:
-                print('Fizz', end='')
-            if i % 5 == 0:
-                print('Buzz', end='')
-
-    print('', end=' ')
+            print("{:d} ".format(num), end="")
