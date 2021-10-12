70f995fcd25ca80611ac22765aee84a5af5dc165
RockoGilbert <evolve2122@gmail.com>
2021-10-12 18:01:15 -0500

    Fixed whitespace

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -9,14 +9,14 @@ class Rectangle:
         Defines Rectangle by width and height
         """
         def __init__(self, width=0, height=0):
-            
+
             self.width = width
             self.height = height
-            
+
         @property
         def width(self):
             return self.__width
-        
+
         @width.setter
         def width(self, value):
             """
@@ -28,11 +28,11 @@ class Rectangle:
             if value < 0:
                 raise ValueError("width must be >= 0")
             self.__width = value
-              
+
         @property
         def height(self):
             return self.__height
-         
+
         @height.setter
         def height(self, value):
             """
@@ -41,6 +41,6 @@ class Rectangle:
             """
             if type(value)is not int:
                 raise TypeError("height must be an integer")
-            if value < 0: 
+            if value < 0:
                 raise ValueError("height must be >= 0")
             self.__height = value
