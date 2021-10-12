b0d5d6a065e7499077844117978ae6037399ec37
RockoGilbert <evolve2122@gmail.com>
2021-10-11 19:16:04 -0500

    A function that adds an integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A function that adds 2 integers
+
+"""
+
+
+def add_integer(a, b=98):
+    """ checks if type is integer if int return sum"""
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    elif type(b) != int:
+        raise TypeError("b must be an integer")
+    else:
+        return a + b
