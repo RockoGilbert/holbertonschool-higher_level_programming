eb3a0f237e4c095a86415304212711ec5a3702bb
RockoGilbert <evolve2122@gmail.com>
2021-10-12 08:47:04 -0500

    Wrote a function that prints a square using #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""prints a square"""
+
+
+def print_square(size):
+    """ Prints a square of length size using the hash (#) character """
+    if type(size) != int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    for i in range(size):
+        print(size * '#')
