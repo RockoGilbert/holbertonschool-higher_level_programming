223c6bbc98dc3af4c5511be805d5166af4ce23fe
RockoGilbert <evolve2122@gmail.com>
2021-10-11 19:30:19 -0500

    changed if else statement

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,7 +12,7 @@ def add_integer(a, b=98):
         b = int(b)
     if type(a) != int:
         raise TypeError("a must be an integer")
-    elif type(b) != int:
+    if type(b) != int:
         raise TypeError("b must be an integer")
     else:
         return a + b
