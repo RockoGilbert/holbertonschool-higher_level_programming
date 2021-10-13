e1e16b5bec7e4ae7362298f381243bff4a430245
RockoGilbert <evolve2122@gmail.com>
2021-10-12 23:28:29 -0500

    Prints a square with a character

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-"""prints a square"""
+""" prints a square with the character #
+"""
 
 
 def print_square(size):
-    """ Prints a square of length size using the hash (#) character """
+    """ Determins the paramaters of size
+        and prints a square with #
+    """
     if type(size) != int:
-        raise TypeError('size must be an integer')
+        raise TypeError("size must be an integer")
+    if type(size) == float and size < 0:
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError('size must be >= 0')
-    for i in range(size):
-        print(size * '#')
+        raise TypeError("size must be >= 0")
+    if size == 0:
+        return None
+
+    for row in range(size):
+        print('#' * size)
