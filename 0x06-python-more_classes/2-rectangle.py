2442d946718098dd49430cefd03763a954b397a3
RockoGilbert <evolve2122@gmail.com>
2021-10-12 20:48:26 -0500

    Fixed error

--- 0x06-python-more_classes/2-rectangle.py
+++ 0x06-python-more_classes/2-rectangle.py
@@ -1,54 +1,70 @@
 #!/usr/bin/python3
-"""creates class rectangle"""
+"""
+Defines Class Rectangle
+"""
 
 
 class Rectangle:
-    """ class rectangle"""
-
+    """
+    Defines Rectangle by width and height
+    """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        width
+        args: value (int)
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
         return self.__height
 
     @height.setter
-    def height(self, height):
-        if type(height) is not int:
+    def height(self, value):
+        """
+        height
+        args: value (int)
+        """
+        if type(value)is not int:
             raise TypeError("height must be an integer")
-        elif height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = height
-
-    @width.setter
-    def width(self, width):
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = width
+        self.__height = value
 
     def area(self):
-        return (self.__width * self.__height)
+        area = self.width * self.height
+        return area
 
     def perimeter(self):
-        if self.__height <= 0 or self.width <= 0:
+        if self.width == 0 or self.height == 0:
             return 0
-        return ((self.__height * 2) + (self.__width * 2))
+        perimeter = 2 * (self.width + self.height)
+        return perimeter
 
     def __str__(self):
-        newString = ''
-        if self.__height == 0 or self.__width == 0:
-            return newString
-        for x in range(self.__height):
-            for y in range(self.__width):
-                    newString += "#"
-                    newString += "\n"
-            return newString[:-1]
+        """
+        __str__
+        returns: printed rectangle
+        """
+        if self.width == 0 or self.height == 0:
+            return ""
+        strg = ""
+        for i in range(self.height - 1):
+            strg += "#" * self.width + "\n"
+            if i is self.height - 2:
+                strg += "#" * self.width
+        return strg
