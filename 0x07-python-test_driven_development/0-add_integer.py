b89e0b4495d5e6b1215b7e20a0ee5ac4210ee3cc
RockoGilbert <evolve2122@gmail.com>
2021-10-12 22:29:38 -0500

    Test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
+"""Adds 2 ints or floats cast ad ints
+"""
+
+
 def add_integer(a, b=98):
+    '''Adds 2 ints or floats and returns them as int
+    Args:
+        a: first value to add
+        b: second value to add
+    Returns:
+        int(a + b)
+    '''
     if type(a) == float or type(b) == float:
         a = int(a)
         b = int(b)
-    if type(a) != int:
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if type(b) != int:
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
+
     else:
-        return a + b
+        return int(round(a) + round(b))
