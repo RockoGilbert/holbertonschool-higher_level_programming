fe15371f6f7ad34d56f00c1d503d412fd0e45efe
RockoGilbert <evolve2122@gmail.com>
2021-10-13 21:55:11 -0500

    Returned an instance of an object

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Returns true if the object is an instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """finds the specified class"""
+    return isinstance(obj, a_class)
