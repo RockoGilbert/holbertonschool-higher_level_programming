da00323afb65d6f935cca5809afa2a8e3abf4ccf
RockoGilbert <evolve2122@gmail.com>
2021-10-13 22:19:26 -0500

    a class bases off a class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write a class based off a file"""
+
+
+class BaseGeometry:
+    """Raises exception"""
+
+
+    def area(self):
+        raise Exception("area() is not implemented")
