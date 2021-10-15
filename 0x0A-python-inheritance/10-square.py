718b89253acd29ec4a4c6c1ef114bcb7d01735c7
RockoGilbert <evolve2122@gmail.com>
2021-10-14 20:23:43 -0500

    test

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Class that inherits from a rectangle"""
+"""Defines a class from an inherited class"""
 
-Rectangle = __import__("9-rectangle").Rectangle
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Creatingthe Class square"""
+    """Creating the class Square"""
 
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.size = size
+        self.__size = size
 
-    def area():
-        return self.__size * self.__size
+        def area(self):
+            return self.__size * self.__size
