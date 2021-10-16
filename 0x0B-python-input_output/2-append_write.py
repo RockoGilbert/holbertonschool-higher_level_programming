603aa273e824818da1bdb6985d336261282f7b39
RockoGilbert <evolve2122@gmail.com>
2021-10-15 21:41:51 -0500

    fixed pep8

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,4 +8,4 @@ def append_write(filename="", text=""):
     
     with open(filename, 'a', encoding='utf=8') as appendFile:
         appendFile.write(text)
-        return len(text)i
+        return len(text)
