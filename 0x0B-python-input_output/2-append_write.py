0c0c6d3421fb6738d3c2afa649b3cbbfe600bdfd
RockoGilbert <evolve2122@gmail.com>
2021-10-15 21:09:59 -0500

    test

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Appends file text
+"""
+
+
+def append_write(filename="", text=""):
+    """appends string and returns count of text"""
+    
+    with open(filename, 'a', encoding='utf=8') as appendFile:
+        appendFile.write(text)
+        return len(text)i
