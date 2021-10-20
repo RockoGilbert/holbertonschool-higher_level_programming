26bf0db244b6e340e0821b68be84c51172ddfef5
RockoGilbert <evolve2122@gmail.com>
2021-10-20 09:47:59 -0500

    Added script arguments to a list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-'''Save all args from list to file'''
+"""add_item
+"""
+
+
 import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-        out = load_from_json_file('add_item.json')
-except:
-        out = []
+        loadFile = load_from_json_file("add_item.json")
+except FileNotFoundError:
+        loadFile = []
 
-save_to_json_file(out + sys.argv[1:], 'add_item.json')
+loadFile += sys.argv[1:]
+
+save_to_json_file(loadFile, 'add_item.json')
