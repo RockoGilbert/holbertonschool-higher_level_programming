7736e6984fa2f4968bda08c5cde6a43cfc2f0d90
RockoGilbert <evolve2122@gmail.com>
2021-10-20 09:46:40 -0500

    Added script arguments to a list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,12 @@
-#!/usr/bin/bash
-"""adds arguments to a Python list"""
-
-
+#!/usr/bin/python3
+'''Save all args from list to file'''
 import sys
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    xfile = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    xfile = []
+        out = load_from_json_file('add_item.json')
+except:
+        out = []
 
-xfile += sys.argv[1:]
-
-save_to_json_file(xfile, 'add_item.json')
+save_to_json_file(out + sys.argv[1:], 'add_item.json')
