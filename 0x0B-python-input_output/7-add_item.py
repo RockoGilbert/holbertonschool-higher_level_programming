437a3b0e28294e5a2c43a69893abdeeecf58e431
RockoGilbert <evolve2122@gmail.com>
2021-10-20 09:45:03 -0500

    Added script arguments to a list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,8 +10,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     xfile = load_from_json_file('add_item.json')
-
-except:
+except FileNotFoundError:
     xfile = []
 
 xfile += sys.argv[1:]
