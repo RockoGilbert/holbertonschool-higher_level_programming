812faaaf7810eaaffa0d228cc5a94a2351c07a70
RockoGilbert <evolve2122@gmail.com>
2021-10-20 09:55:26 -0500

    Serialization of an object

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Returns the dictionary description or an object"""
+
+
+def class_to_json(obj):
+ """Attributes
+
+    Args: 
+        obj: obj to return the __dict__ of
+
+ """
+return obj.__dict__
