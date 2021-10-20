24ff3a566d230ab5060221ebf32eacc112b7df09
RockoGilbert <evolve2122@gmail.com>
2021-10-20 17:53:19 -0500

    fixed errors

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+
+class Student
+"""Student Info"""
+
+
+def __init__(self, first_name, last_name, age)
+"""Creates Student"""
+
+self.first_name = first_name
+self.last_name = last_name
+self.age = age
+
+
+def to_json(self):
+
+    """Returns dict representation of obj"""
+
+    return self.__dict__
