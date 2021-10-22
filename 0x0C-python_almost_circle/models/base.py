e695c9887a91c0e88e9aaec215f0c8c614d15882
RockoGilbert <evolve2122@gmail.com>
2021-10-22 09:46:09 -0500

    test

--- 0x0C-python_almost_circle/models/base.py
+++ 0x0C-python_almost_circle/models/base.py
@@ -19,12 +19,15 @@ class Base:
             self.id = Base.__nb_objects
 
     def integer_validator(self, name, value):
+        """integer_validator"""
+
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
     def xyValidator(self, name, value):
+        """xyValidator"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value < 0:
@@ -32,8 +35,8 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Returns JSON string representation
-        """
+        """Returns JSON string representation"""
+
         newLd = []
         if list_dictionaries is None:
             return newLd
@@ -43,48 +46,50 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Writes to file with JSON string
-        """
-        with open(cls.__name__ + ".json", 'w') as write_file:
-            if list_objs is None:
-                write_file.write('[]')
-            else:
-                write_file.write(cls.to_json_string([item.to_dictionary()
-                                                     for item in list_objs]))
+        """saves a dict in json to a new file"""
+        if list_objs is None:
+            list_objs = []
+
+        filename = cls.__name__ + '.json'
+        newList = []
+
+        for item in list_objs:
+            newList.append(item.to_dictionary())
+
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(newList))
 
     @staticmethod
     def from_json_string(json_string):
-        """Returns JSON strings in list
-        """
-        if json_string is None:
-            return ([""])
-        else:
-            return json.loads(json_string)
+        """Json to dictionary"""
+        if json_string == None:
+            return []
+        return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """returns an instance with all attributes already set
-        """
-        if cls.__name__ == 'Rectangle':
-            dummy = cls(1, 1)
-        elif cls.__name__ == 'Square':
-            dummy = cls(1)
+        """Creates a new class"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1, 0, 0)
+
+        if cls.__name__ == "Square":
+            dummy = cls(1, 0, 0)
+
         dummy.update(**dictionary)
         return dummy
 
     @classmethod
     def load_from_file(cls):
-        lists = []
-        with open(cls.__name__ + '.json', 'r') as readFile:
-            text = readFile.read()
+        """Loads from a file"""
+        filename = cls.__name__ + '.json'
+        newList = []
 
-        text = cls.from_json_string(text)
-        print('text', text)
-        for i in text:
-            print('i', i)
+        try:
+            with open(filename, 'r') as f:
+                newList = cls.from_json_string(f.read())
+                for i, j in enumerate(newList):
+                    newList[i] = cls.create(**newList[i])
+            return newList
 
-            if type(i) == dict:
-                lists.append(cls.create(**i))
-            else:
-                lists.append(i)
-            return lists
+        except:
+            return []
