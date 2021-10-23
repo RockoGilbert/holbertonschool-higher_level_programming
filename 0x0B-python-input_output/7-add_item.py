702b2fc6220f285df20adfe71999803e6d8de54f
RockoGilbert <evolve2122@gmail.com>
2021-10-22 19:02:49 -0500

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 '''Save all args from list to file'''
+
 import sys
+import json
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-        out = load_from_json_file('add_item.json')
+    add_json = load_from_json_file('add_item.json')
 except:
-        out = []
+    add_json = []
 
-save_to_json_file(out + sys.argv[1:], 'add_item.json')
+add_json += sys.argv[1:]
+
+save_to_json_file(add_json, 'add_item.json')i
