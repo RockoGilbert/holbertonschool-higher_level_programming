4d1f7b73c92add4affcce8e386cb3be48589b367
RockoGilbert <evolve2122@gmail.com>
2021-10-22 19:13:30 -0500

    Fixed pep8 errors

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-"""Student Class"""
+"""This module defines class Student"""
 
 
-class Student
-"""Student Info"""
+class Student:
+    """Adding definition to class"""
 
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-def __init__(self, first_name, last_name, age)
-"""Creates Student"""
-
-self.first_name = first_name
-self.last_name = last_name
-self.age = age
-
-
-def to_json(self):
-
-    """Returns dict representation of obj"""
-
-    return self.__dict__
+    def to_json(self):
+        return vars(self)
