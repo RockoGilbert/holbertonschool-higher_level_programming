f562ca5bbbd203f71453a23a96c3c3e18dca3199
RockoGilbert <evolve2122@gmail.com>
2021-10-22 19:51:22 -0500

    test

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
-"""
-Write a class Student that defines a student by: (based on 10-student.py)
-"""
+'''Module for Student class'''
 
 
 class Student:
-    """class student student"""
+    '''Student class'''
 
     def __init__(self, first_name, last_name, age):
-        """class specifications for student"""
+        '''Creates a Student
+
+        Args:
+            first_name: the student's first name
+            last_name: the student's last name
+            age: the student's age
+        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves dictonary reps of student class"""
+        '''Returns dictionary representation of obj
 
-        strs = {}
-
+        Args:
+            attrs: list of keys to JSONify
+        '''
         if attrs is not None:
-            for varis in attrs:
-                if varis in self.__dict__:
-                    strs[varis] = self.__dict__[varis]
-                return strs
-            else:
-                return vars(self)
+            return {k: self.__dict__[k] for k in self.__dict__ if k in attrs}
+        else:
+            return self.__dict__
 
     def reload_from_json(self, json):
-        self.__dict__ = json
+        '''Resets attributes in Student based on JSON input
+
+        Args:
+            json: dictionary object
+        '''
+        for k in json:
+            setattr(self, k, json[k])
