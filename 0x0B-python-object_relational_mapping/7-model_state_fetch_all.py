8b6b689558f1abc98c9c976f4314f31d0bbf1c14
RockoGilbert <evolve2122@gmail.com>
2021-11-17 15:44:40 -0600

    Corrected the meta data engine

--- 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-'''
-Lists all State objects from the database hbtn_0e_6_usa
-'''
-from model_state import Base, State
-import sys
-import sqlalchemy as sa
-from sqlalchemy import (create_engine)
-
+'''orm module'''
 
 if __name__ == '__main__':
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    conn = engine.connect()
-    metadata = sa.MetaData()
-    states = sa.Table('states', metadata, autoload=True, autoload_with=engine)
-    query = sa.select([states])
+    from sqlalchemy.engine import create_engine
+    from sqlalchemy.orm.session import sessionmaker
+    from model_state import State
+    from model_state import Base, State
+    from sys import argv
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]))
 
-    ResultProxy = conn.execute(query)
-
-    ResultSet = ResultProxy.fetchall()
-
-    for key, value in ResultSet:
-        print("{}: {}".format(key, value))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    for state in session.query(State).order_by(State.id):
+        print(state)
