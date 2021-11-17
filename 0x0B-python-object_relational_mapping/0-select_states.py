e50ad747a1ffb88e8d5dac50054e89b101897e8d
RockoGilbert <evolve2122@gmail.com>
2021-11-17 11:01:41 -0600

    test 1 2

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-"""Lists all state from the database hbtn_0e_0_usa"""
+""" lists all states from the database hbtn_0e_0_usa """
+
+from sys import argv
 import MySQLdb
-import sys
-
-
-def get_states():
-    """Take arguments argv to list from database
-    Arguments:
-        argv[1]: mysql username
-        argv[2]: mysql password
-        argv[3]: database name
-    """
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3])
-
-    cur = conn.cursor()
-
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    for row in cur.fetchall():
-        print(row)
-    cur.close()
-    conn.close()
-
 
 if __name__ == "__main__":
-    get_states()
+
+    try:
+        args = {
+            "user": argv[1],
+            "passwd": argv[2],
+            "db": argv[3],
+            "host": "localhost",  # This is default MySQLdb value
+            "port": 3306        # This is default MySQLdb value
+        }
+        QUERY = "SELECT * FROM states ORDER BY states.id"
+
+        # Connect to database, execute QUERY
+        db_connection = MySQLdb.connect(**args)
+        cursor = db_connection.cursor()
+        cursor.execute(QUERY)
+        tuples = cursor.fetchall()
+
+        # Clean up
+        cursor.close()
+        db_connection.close()
+
+        # Print result
+        for tuple in tuples:
+            print(tuple)
+    except:
+        pass
