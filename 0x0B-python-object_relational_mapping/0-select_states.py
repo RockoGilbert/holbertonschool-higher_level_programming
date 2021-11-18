1e1ab7e06baf58f0d4d0c6f3099d884843d3e10a
RockoGilbert <evolve2122@gmail.com>
2021-11-18 08:21:38 -0600

    test

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
 """Lists all state from the database hbtn_0e_0_usa"""
+
+
 import MySQLdb
 import sys
 
 
-def get_states():
-    """Take arguments argv to list from database
-    Arguments:
-        argv[1]: mysql username
-        argv[2]: mysql password
-        argv[3]: database name
-    """
+if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
-
     cur = conn.cursor()
-
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
     for row in cur.fetchall():
         print(row)
     cur.close()
     conn.close()
-
-
-if __name__ == "__main__":
-    get_states()
