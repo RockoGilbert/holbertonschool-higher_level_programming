fdd3d02014ed6401431df9be0cac186d22d41f83
RockoGilbert <evolve2122@gmail.com>
2021-11-18 13:25:05 -0600

    Edited query statement

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -7,11 +7,11 @@ from sys import argv
 if __name__ == '__main__':
     """Access to the database"""
     db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        host="localhost",
+        port=3306
     )
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
