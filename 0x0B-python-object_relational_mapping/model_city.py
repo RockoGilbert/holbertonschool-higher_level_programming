97114516ce88f445febdbfa9045d95f0280960bf
RockoGilbert <evolve2122@gmail.com>
2021-11-17 22:36:23 -0600

    A script that searches cities

---
+++ 0x0B-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+State Object mapping to states table
+"""
+from sqlalchemy import Column
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """State table representation"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('State.id'), nullable=False)
+    
