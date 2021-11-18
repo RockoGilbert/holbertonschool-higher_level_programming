cadcc8a74c2142a36f3772bf305a5edc7c023aa1
RockoGilbert <evolve2122@gmail.com>
2021-11-18 13:35:52 -0600

    change p code

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Lists all state from the database hbtn_0e_0_usa"""
+"""List all state from the database hbtn_0e_0_usa"""
 
 
 import MySQLdb
 import sys
-
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
