cbaf5397b4933e3d5fd99cf328c3d87adfdc5367
RockoGilbert <evolve2122@gmail.com>
2022-01-06 13:57:27 -0600

    Went back and added text inside the strings

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const stringz = ['C is fun', 'Python is cool', 'Javascript'];
+const stringz = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < stringz.length; i++) {
     console.log(stringz[i]);
 }
