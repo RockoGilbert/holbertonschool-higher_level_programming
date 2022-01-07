c0a28a82ef37702a4d68c4af762534edb390b53d
RockoGilbert <evolve2122@gmail.com>
2022-01-07 13:39:31 -0600

    fixed errors

--- 0x01-javascript_objects_scopes_closures/5-square.js
+++ 0x01-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-//A class sq that defines a square and inherits from Rectangle
+// A class sq that defines a square and inherits from Rectangle
 
 const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
-  constructor(size) {
+  constructor (size) {
     super(size, size);
   }
-}
+};
