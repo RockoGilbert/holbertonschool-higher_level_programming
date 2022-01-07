3ea8dd0320f1679ee9accc7eb5c1a39ca03926d4
RockoGilbert <evolve2122@gmail.com>
2022-01-07 14:04:04 -0600

    changed the constructor logic

--- 0x01-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-// Writing a class rectangle that defines a rectangle
 
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
-
-    if (w < 1 || h < 1) { return; }
-    if (w === undefined || h === undefined) { }
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 }
-
 module.exports = Rectangle;
