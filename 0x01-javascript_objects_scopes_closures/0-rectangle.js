431eb233721b1013c8167e190d5ca963edfc2a62
RockoGilbert <evolve2122@gmail.com>
2022-01-07 08:22:03 -0600

    Created an empty Rectangle Class

--- 0x01-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
+// Write an empty class Rectangle that defines a rectangle
 
-class Rectangle {
-    constructor(x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-      }
-
-      toString (){
-         return `[x = ${this.x}, y = ${this.y}, width = ${this.width}, height = ${this.height}]`
-        }
-    };
-
-    let zRect = new Rectangle(2, 2, 4, 4);
-    console.log(zRect.toString());
-    
+class Rectangle {}
+module.exports = Rectangle;
