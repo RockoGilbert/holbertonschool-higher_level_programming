ee2d3214bd2543cfebb74f5304eb0e2baa440f93
RockoGilbert <evolve2122@gmail.com>
2022-01-11 15:23:28 -0600

    Wrote a script that gets the content of a webpage

---
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+require('request').get(process.argv[2], function (err, r, body) {
+  if (err) {
+    fs.writeFile(process.argv[3], body, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+  });
