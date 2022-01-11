360b70ce4ccf912e167a82df1c299204110d24f1
RockoGilbert <evolve2122@gmail.com>
2022-01-11 12:15:40 -0600

    fixed semistandard

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-const fs = require('fs'); 
-const data= process.argv[3];
+const fs = require('fs');
+const data = process.argv[3];
 
 fs.writeFile(process.argv[2], data, 'utf-8', (err) => {
   if (err) {
     console.log(err);
   }
-
 });
