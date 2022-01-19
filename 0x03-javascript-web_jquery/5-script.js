7786387d29210b4529d7edbd25e2738b193cb1b6
RockoGilbert <evolve2122@gmail.com>
2022-01-19 11:00:28 -0600

    Added the append function to add li item

--- 0x03-javascript-web_jquery/5-script.js
+++ 0x03-javascript-web_jquery/5-script.js
@@ -1,3 +1,5 @@
-$("#add_item").click(function() {
-  $(".my_list").append("<li>Item</li>");
-});
+$(function() {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  })
+})
